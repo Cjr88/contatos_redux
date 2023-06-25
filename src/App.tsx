d4c0d8589ea6ffe664d9381f8c1ebae82eba1fe8
Cjr88 <carloscjr70@gmail.com>
2023-06-25 16:46:14 -0300

    setup inicial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import MainContainer from './styles'
+
+function App() {
+  return (
+    <>
+      <MainContainer />
+      <h1>Setup basico</h1>
+      <p>Hello, world!</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/styles/index.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const MainContainer = styled.div`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+    font-family: 'Roboto', sans-serif;
+    font-family: 'Roboto Condensed', sans-serif;
+    font-family: 'Roboto Mono', monospace;
+    font-family: 'Ysabeau Infant', sans-serif;
+    font-family: 'Ysabeau Office', sans-serif;
+    list-style: none;
+  }
+`
+export default MainContainer
